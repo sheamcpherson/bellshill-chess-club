b59e55c03b0264cce1d84cb8ecadac17e1255590
sheamcpherson <sheamcpherson@outlook.com>
2025-08-11 21:16:59 +0100

    fixed routing links on home

--- bellshill-chess-club/src/app/shell/pages/home/home.component.ts
+++ bellshill-chess-club/src/app/shell/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+    styleUrls: ['./home.component.css'],
+    imports: [RouterModule],
+    standalone: true
 })
 export class HomeComponent {
     
